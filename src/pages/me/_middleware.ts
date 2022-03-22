ee4362cb4ca5126c75fc385fdf0c371f4465f96f
LMarcel <lmgh1312@gmail.com>
2022-03-22 17:02:55 -0300

    Adicionando log para encontrar erros no middleware

--- src/pages/me/_middleware.ts
+++ src/pages/me/_middleware.ts
@@ -2,12 +2,16 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req: Req) {
+  console.log(req, process.env.NEXTAUTH_SECRET);
   const token = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET
   });
 
+  console.log(token);
+
   if(!token) {
+    console.log("bug!");
     return NextResponse.redirect("/error?type=401");
   };
 
